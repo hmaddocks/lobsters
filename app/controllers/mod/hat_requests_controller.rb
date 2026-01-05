6e2d32c6274b340f53886bb6801a92c20b93cb2c
Henry Maddocks <hmaddocks@me.com>
2026-01-05 13:28:10 +1300

    Create the mod/hat_requests controller and views

---
+++ app/controllers/mod/hat_requests_controller.rb
@@ -0,0 +1,31 @@
+# typed: false
+
+class Mod::HatRequestsController < Mod::ModController
+  before_action :require_logged_in_moderator
+  before_action :show_title_h1
+
+  def index
+    @title = "Hat Requests"
+    @hat_requests = HatRequest.all.includes(:user)
+  end
+
+  def approve
+    @hat_request = HatRequest.find(params[:id])
+    @hat_request.update!(params.require(:hat_request)
+      .permit(:hat, :link, :reason).except(:reason))
+    @hat_request.approve_by_user_for_reason!(@user, params[:hat_request][:reason])
+
+    flash[:success] = "Successfully approved hat request."
+
+    redirect_to hat_requests_path
+  end
+
+  def reject
+    @hat_request = HatRequest.find(params[:id])
+    @hat_request.reject_by_user_for_reason!(@user, params[:hat_request][:reason])
+
+    flash[:success] = "Successfully rejected hat request."
+
+    redirect_to hat_requests_path
+  end
+end
